971ae76de438ad01385d5667d8fac99125df1716
Jayesh Khairnar <jayeshkhairnar6@gmail.com>
2024-06-21 12:07:11 +0530

    markdown basic code

---
+++ src/MarkdownEditor.js
@@ -0,0 +1,36 @@
+import { useState, Suspense, lazy } from 'react';
+const MarkdownPreview = lazy(() => delayForDemo(import('./MarkdownPreview.js')));
+export default function MarkdownEditor() {
+
+    const [showPreview, setShowPreview] = useState(false);
+
+    return (
+        <>
+            <label>
+                <input type="checkbox" checked={showPreview} onChange={e => setShowPreview(e.target.checked)} />
+                Show preview
+            </label>
+            <hr />
+            {showPreview && (
+                <Suspense fallback={<Loading />}>
+                    <h2>Preview</h2>
+                    <MarkdownPreview />
+                </Suspense>
+            )}
+        </>
+    );
+}
+// Add a fixed delay so you can see the loading state
+
+function delayForDemo(promise) {
+    return new Promise(resolve => {
+        setTimeout(resolve, 2000);
+    }).then(() => promise);
+}
+function Loading() {
+    return <p><i>Loading...</i></p>;
+}
+
+
+
+
---
+++ src/MarkdownPreview.js
@@ -0,0 +1,6 @@
+export default function MarkdownPreview() {
+    return (<div>
+        Hello....
+    </div>
+    );
+}
